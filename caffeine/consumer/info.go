b0bd27d4760980f41a73055de8c4de913be6780c
designerasun <asuntheawesome@gmail.com>
2021-07-28 00:58:45 +0900

    caffeine checker module 1 - calculating caffeine amount based on product

--- caffeine/consumer/info.go
+++ caffeine/consumer/info.go
@@ -7,42 +7,62 @@ import "fmt"
 // 1) age : 1 ~ 19 - teenager // 20 ~ 60 adult // 61 ~ 90 senior
 // 2) sex : man // woman
 // 3) pregnancy : T/F
+// Function: 1) InfoEnter 2) Recommended amount 3) Actual amount 4) Compare amounts
 
-type User struct {
-	name   string
-	age    int
-	sex    string
-	weight float64
-	isPreg bool
+// Create a map to index coffee drinks and corresponding caffeine amount.
+var Drinks = map[string]float64{
+	"LetsBe":  20,
+	"Georgia": 10,
+	"Maxim":   30,
 }
 
-func (u *User) EnterInfo(name, sex string, age int, weight float64, isPreg bool) {
-	u.name = name
-	u.sex = sex
-	u.age = age
-	u.isPreg = isPreg
-	u.weight = weight
+// Create an abstract class : CaffeineCheck
+type CaffeineCheck interface {
+	Calculate(float64) float64
+	ChooseCoffee(string)
 }
 
-func (u *User) DecideCaffeine() (amount float64) {
-	if u.age <= 19 {
-		amount = u.weight * 2.5
-		return amount
+// Create a embodying class : Coffee
+type Coffee struct {
+	product    string
+	caffAmount float64
+}
 
-	} else if u.age <= 60 && u.isPreg { // default bool value: false
-		amount = 400
-		return amount
+// Calculate total caffeine amount
+func (c *Coffee) Calculate(number float64) float64 {
+	total := c.caffAmount * number
+	return total
+}
+
+// Choose which coffee drink to have
+func (c *Coffee) ChooseCoffee(name string) {
+
+	c.product = name
+
+	if c.product == "LetsBe" {
+		c.caffAmount += Drinks["LetsBe"]
+
+	} else if c.product == "Georgia" {
+		c.caffAmount += Drinks["Georgia"]
 
 	} else {
-		amount = 300
-		return amount
+		c.caffAmount += Drinks["Maxim"]
 	}
 
 }
 
 func main() {
+	// Declare a Coffee strcuture variable
+	var myCaffeine Coffee
 
-	fmt.Println("hello world")
+	// Choose drinks from the map
+	myCaffeine.ChooseCoffee("Georgia")
+
+	// Calculate total caffeine amount
+	result := myCaffeine.Calculate(5)
+
+	fmt.Println(result)
+
 }
 
 // Interface:
@@ -55,3 +75,36 @@ func main() {
 // 2. Depending on the struct, it categorizes which one is which.
 // 3. Turn the functionA into a method connected to a new structA(embodying class).
 // 4. Now you can declare a new variable for the new structA and use all the interface functionality.
+
+// type User struct {
+// 	name   string
+// 	age    int
+// 	sex    string
+// 	weight float64
+// 	isPreg bool
+// 	info []string
+// }
+
+// func (u *User) EnterInfo(name, sex string, age int, weight float64, isPreg bool) {
+// 	u.name = name
+// 	u.sex = sex
+// 	u.age = age
+// 	u.isPreg = isPreg
+// 	u.weight = weight
+// }
+
+// func (u *User) DecideCaffeineCheck() (amount float64) {
+// 	if u.age <= 19 {
+// 		amount = u.weight * 2.5
+// 		return amount
+
+// 	} else if u.age <= 60 && u.isPreg { // default bool value: false
+// 		amount = 400
+// 		return amount
+
+// 	} else {
+// 		amount = 300
+// 		return amount
+// 	}
+
+// }
