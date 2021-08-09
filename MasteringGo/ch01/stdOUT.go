081ac288e911a22109127d387d5c65b64fe375d0
designerasun <asuntheawesome@gmail.com>
2021-08-09 22:32:33 +0900

    file descriptor of UNIX - stdOUT

--- MasteringGo/ch01/stdOUT.go
+++ MasteringGo/ch01/stdOUT.go
@@ -7,13 +7,17 @@ import (
 
 func main() {
 	myString := ""
+
+	// os package is used to read cli args.
 	arguments := os.Args
 	if len(arguments) == 1 {
-		myString = "Please give me one argument!"
+		myString = "Please give me one argument!" // a hardcoded message
 	} else {
 		myString = arguments[1]
 	}
 
+	// stdOUT.go uses the io package instead of fmt package.
+	// io.WriteString takes two args: 1) os.File(io.Writer) 2) string value
 	io.WriteString(os.Stdout, myString)
 	io.WriteString(os.Stdout, "\n")
 }
