8220992d11a03ff71077221a6992f480b31a290a
designerasun <asuntheawesome@gmail.com>
2021-08-14 00:55:45 +0900

    garbage collector detail added

--- MasteringGo/ch02/sliceGC.go
+++ MasteringGo/ch02/sliceGC.go
@@ -15,7 +15,11 @@ func main() {
 		value := int(i)
 		structure = append(structure, data{value, value})
 	}
-
+	// GC runs a garbage collection and blocks the caller until the garbage collection is complete.
+	// It may also block the entire program.
 	runtime.GC()
+
+	// Below code is used to prevent Go garbage collector from garbage-collecting the data struct too early
+	// even before it is referenced to.
 	_ = structure[0]
 }
