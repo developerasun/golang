3b76bf8927ae9bc582bbf88e1aac6edf6a5b9c29
designerasun <asuntheawesome@gmail.com>
2021-08-15 13:41:10 +0900

    pointer restrictions in Go

--- pointer/point.go
+++ pointer/point.go
@@ -8,6 +8,27 @@ func main() {
 	// Function ChangeX takes myX's memory address as argument
 	ChangeX(&myX)
 	fmt.Println("The value of X is: ", myX)
+
+	// Unlike pointer in C, pointer in Go does have some more constraints
+
+	a := int(5) // type assertion
+	p := &a
+	// p++ : No mathematic operations allowed on pointer.
+	fmt.Print(p)
+
+	floatA := 5.0
+	floatP := &floatA
+	// floatP = &a : Cannot convert between different types of pointers
+	fmt.Print(floatP)
+
+	// fmt.Print(p == floatP) : Cannot compare between different types of pointers
+
+	// In unsafe package there is a pointer defined, which can bypass type system check.
+	// Most of time, use of unsafe pointer is not recommended since as its name suggests, it is unsafe.
+
+	// Two important features of unsafe package : 1) any pointer <-> unsafe.Pointer 2) unsafe.Pointer <-> uintptr
+	// On pointer level, no mathematical operations supported(in golang)
+	// Use uintptr if pointer-wise mathematical operations needed.
 }
 
 // Referencing variable X to see a value
