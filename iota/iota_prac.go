47e9020694c80096486bc2060ef7ef07998117b0
designerasun <asuntheawesome@gmail.com>
2021-12-23 13:40:53 +0900

    understanding enum/iota in Go

--- iota/iota_prac.go
+++ iota/iota_prac.go
@@ -5,16 +5,17 @@ import "fmt"
 const (
 	// iota starts with value 0 and increase ++1 each time. It is similar to enum function in other languages.
 	// Use iota to increase code readability by assigning predefined numbers to const.
-	Sunday    = 1 << iota
-	Monday    // 1 << 1
-	Tuesday   // 1 << 2
-	Wednesday // 1 << 3
-	Thursday  // 1 << 4
-	Friday    // 1 << 5
-	Saturday  // 1 << 6
+	Sunday    = iota // 0
+	Monday    // 1
+	Tuesday   // 2
+	Wednesday // 3
+	Thursday  // 4
+	Friday    // 5
+	Saturday  // 6
 )
 
 func main() {
-	stat := Tuesday
+	stat := Saturday
+
 	fmt.Println(stat)
 }
